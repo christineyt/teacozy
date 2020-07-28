df8f4ead70a445cd2c6065004c18d979590e3cf1
Christine Chan <66407656+christineyt@users.noreply.github.com>
2020-07-28 11:25:17 +0800

    Add files via upload
    initial

---
+++ js/index.js
@@ -0,0 +1,100 @@
+function changePhoto1(){
+    document.querySelector("#img001").src="./images/change1.jpg";
+}
+
+function changePhoto2(){
+    document.querySelector("#img001").src="./images/img1.jpg";
+}
+
+function changePhoto3(){
+    document.querySelector("#img002").src="./images/change2.jpg";
+}
+
+function changePhoto4(){
+    document.querySelector("#img002").src="./images/img2.jpg";
+}
+
+function changePhoto5(){
+    document.querySelector("#img003").src="./images/change3.jpg";
+}
+
+function changePhoto6(){
+    document.querySelector("#img003").src="./images/img3.jpg";
+}
+
+function changePhoto7(){
+    document.querySelector("#img004").src="./images/change4.jpg";
+}
+
+function changePhoto8(){
+    document.querySelector("#img004").src="./images/img4.jpg";
+}
+
+function changePhoto9(){
+    document.querySelector("#img005").src="./images/change5.jpg";
+}
+
+function changePhoto10(){
+    document.querySelector("#img005").src="./images/img5.jpg";
+}
+
+function changePhoto11(){
+    document.querySelector("#img006").src="./images/change6.jpg";
+}
+
+function changePhoto12(){
+    document.querySelector("#img006").src="./images/img6.jpg";
+}
+
+function changePhoto13(){
+    document.querySelector("#img007").src="./images/change7.jpg";
+}
+
+function changePhoto14(){
+    document.querySelector("#img007").src="./images/img7.jpg";
+}
+
+function changePhoto15(){
+    document.querySelector("#img008").src="./images/change8.jpg";
+}
+
+function changePhoto16(){
+    document.querySelector("#img008").src="./images/img8.jpg";
+}
+
+function changePhoto17(){
+    document.querySelector("#img009").src="./images/change9.jpg";
+}
+
+function changePhoto18(){
+    document.querySelector("#img009").src="./images/img9.jpg";
+}
+
+
+
+document.querySelector("#img001").onmouseout = changePhoto1;
+document.querySelector("#img001").onmouseenter = changePhoto2;
+
+document.querySelector("#img002").onmouseout = changePhoto3;
+document.querySelector("#img002").onmouseenter = changePhoto4;
+
+document.querySelector("#img003").onmouseout = changePhoto5;
+document.querySelector("#img003").onmouseenter = changePhoto6;
+
+document.querySelector("#img004").onmouseout = changePhoto7;
+document.querySelector("#img004").onmouseenter = changePhoto8;
+
+document.querySelector("#img005").onmouseout = changePhoto9;
+document.querySelector("#img005").onmouseenter = changePhoto10;
+
+document.querySelector("#img006").onmouseout = changePhoto11;
+document.querySelector("#img006").onmouseenter = changePhoto12;
+
+document.querySelector("#img007").onmouseout = changePhoto13;
+document.querySelector("#img007").onmouseenter = changePhoto14;
+
+document.querySelector("#img008").onmouseout = changePhoto15;
+document.querySelector("#img008").onmouseenter = changePhoto16;
+
+document.querySelector("#img009").onmouseout = changePhoto17;
+document.querySelector("#img009").onmouseenter = changePhoto18;
